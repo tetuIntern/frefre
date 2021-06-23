9c32b60507af1d3d29d9de930844204a159365fd
tetuIntern <p-tetsunosuke.shimoda@freegian.co.jp>
2021-06-23 19:36:45 +0900

    final

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  mode: 'universal',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-first',
